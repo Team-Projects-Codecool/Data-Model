987abecf677284cd7f050cf23a9ef68cf3dcb229
ola-sk <50403262+ola-sk@users.noreply.github.com>
2023-03-01 18:53:17 +0100

    Add access levels to group, space and device.Refactor, Add missing parts.

--- Homee General Data Schema.sql
+++ Homee General Data Schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user (
+CREATE TABLE a_user (
   id SERIAL PRIMARY KEY,
   username VARCHAR(50) UNIQUE NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE device (
   purchase_price NUMERIC(10, 2),
   created_at TIMESTAMP DEFAULT now(),
   updated_at TIMESTAMP DEFAULT now(),
-  owner_id integer REFERENCES user(id),
+  owner_id integer REFERENCES a_user(id) REFERENCES a_user(id) ON DELETE CASCADE,
   about TEXT,
 );
 
@@ -40,33 +40,51 @@ CREATE TABLE technician (
 CREATE TABLE space (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
+  owner_id integer NOT NULL REFERENCES a_user(id) ON DELETE CASCADE,
   about TEXT,
+  UNIQUE (owner_id, name)
 );
 
-CREATE TABLE space_group (
+CREATE TABLE group (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  about TEXT
+  owner_id integer NOT NULL REFERENCES a_user(id) ON DELETE CASCADE,
+  about TEXT,
+  UNIQUE (owner_id, name)
+);
+
+CREATE TABLE user_group_access (
+  user_id integer REFERENCES a_user(id) ON DELETE CASCADE,
+  group_id integer REFERENCES group(id) ON DELETE CASCADE,
+  access_level smallint,
+  PRIMARY KEY (user_id, group_id)
 );
 
 CREATE TABLE user_space_access (
-  user_id integer REFERENCES user(id) ON DELETE CASCADE,
+  user_id integer REFERENCES a_user(id) ON DELETE CASCADE,
   space_id integer REFERENCES space(id) ON DELETE CASCADE,
+  access_level smallint,
   PRIMARY KEY (user_id, space_id)
 );
 
 CREATE TABLE user_device_access (
-  user_id integer REFERENCES user(id) ON DELETE CASCADE,
+  user_id integer REFERENCES a_user(id) ON DELETE CASCADE,
   device_id bigint REFERENCES device(id) ON DELETE CASCADE,
+  access_level smallint,
   PRIMARY KEY (user_id, device_id)
 );
 
-CREATE TABLE space_device_access (
+CREATE TABLE space_group (
   space_id integer REFERENCES space(id) ON DELETE CASCADE,
-  device_id bigint REFERENCES device(id) ON DELETE CASCADE,
-  PRIMARY KEY (space_id, device_id)
+  group_id bigint REFERENCES group(id) ON DELETE CASCADE,
+  PRIMARY KEY (space_id, group_id)
 );
 
+CREATE TABLE device_space (
+  device_id bigint REFERENCES device(id) ON DELETE CASCADE,
+  space_id integer REFERENCES space(id) ON DELETE CASCADE,
+  PRIMARY KEY (device_id, space_id)
+);
 
 CREATE TABLE technician_device_access (
   technician_id integer REFERENCES technician(id) ON DELETE CASCADE,
@@ -74,21 +92,14 @@ CREATE TABLE technician_device_access (
   PRIMARY KEY (technician_id, device_id)
 );
 
-
 CREATE TABLE user_group_access (
   user_id integer REFERENCES user(id) ON DELETE CASCADE,
   group_id integer REFERENCES space_group(id) ON DELETE CASCADE,
   PRIMARY KEY (user_id, group_id)
 );
 
-CREATE TABLE space_space_group_mapping (
-  space_group_id integer REFERENCES space_group(id) ON DELETE CASCADE, -- automatically delete any associated rows in the space_space_group_mapping table when a space_group row is deleted.
-  space_id integer REFERENCES space(id) ON DELETE CASCADE, -- automatically delete any associated rows in the space_space_group_mapping table when a space row is deleted.
-  PRIMARY KEY (space_group_id, space_id)
-);
-
--- This function automatically deletes a space group if it has got empty (no more mappings with any group)
-CREATE OR REPLACE FUNCTION delete_space_group_if_no_mappings()
+-- This function automatically deletes a group if it has got empty (no more mappings with any space)
+CREATE OR REPLACE FUNCTION delete_group_if_no_mapping()
 RETURNS TRIGGER AS $$
 BEGIN
     IF NOT EXISTS (SELECT 1 FROM space_space_group_mapping WHERE space_group_id = OLD.space_group_id) THEN
@@ -97,3 +108,8 @@ BEGIN
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_group_if_last_mapping_deleted_trigger
+AFTER DELETE ON space_group
+FOR EACH ROW
+EXECUTE FUNCTION delete_group_if_no_mapping();
